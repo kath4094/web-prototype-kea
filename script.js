dcb30e7224144592504939dc3dbcbfe98123650f
Kathrine Brohus <kathrinebrohus@Kathrines-MacBook-Pro-2.local>
2019-05-28 14:51:55 +0200

    Login, forside og burgermenu done

---
+++ script.js
@@ -0,0 +1,25 @@
+window.addEventListener("load", sidenVises);
+
+function sidenVises() {
+    console.log("sidenVises");
+
+    document.querySelector("#menuknap").addEventListener("click", toggleMenu);
+
+
+}
+
+
+function toggleMenu() {
+    console.log("toggleMenu");
+    document.querySelector("#menu_list").classList.toggle("hidden");
+
+    document.querySelector("#profile_menu").classList.toggle("hidden");
+
+    let erSkjult = document.querySelector("#menu_list").classList.contains("hidden");
+
+    if (erSkjult == true) {
+        document.querySelector("#menuknap").textContent = "☰";
+    } else {
+        document.querySelector("#menuknap").textContent = "X";
+    }
+}
